58894e55e15f02e12d96814bf8c18b7fd45ac019
Nusendra Hanggarawan <admin@nusendra.com>
2019-10-04 22:12:22 +0700

    added butterfly pattern

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import triangle1 from './triangle1';
 import triangle2 from './triangle2';
+import butterflyTie from './butterflyTie';
 
 triangle1(5, "*");
 triangle2(5, "*");
+butterflyTie(5, "#");
