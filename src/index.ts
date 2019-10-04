20f8fd5d61faccc88b350792d16745e69947a0ff
Nusendra Hanggarawan <admin@nusendra.com>
2019-10-04 21:46:02 +0700

    added triangle2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import triangle1 from './triangle1';
+import triangle2 from './triangle2';
 
 triangle1(5, "*");
+triangle2(5, "*");
---
+++ src/triangle2.ts
@@ -0,0 +1,30 @@
+/**
+ * Expectations
+ * 
+ * *****
+ * ****
+ * ***
+ * **
+ * *
+ * 
+ * @param digits number
+ * @param pattern string 
+ * 
+ * @returns string
+ */
+const triangle1 = (digits: number, pattern: string = "*"): void => {
+    console.log("------ TRIANGLE 2 ------");
+
+    for (let index = 5; index > 0; index--) {
+        let result: string = "";
+
+        for (let subIndex = 0; subIndex < index; subIndex++) {
+            result += pattern;
+        }
+        console.log(result);
+    }
+
+    console.log(" ");
+}
+
+export default triangle1
